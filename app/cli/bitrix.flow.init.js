1d72907b9fa04722186aa804ef7441b890f9d8e0
Vladimir Belov <>
2018-11-26 11:55:31 +0200

    Fix flow:init

--- app/cli/bitrix.flow.init.js
+++ app/cli/bitrix.flow.init.js
@@ -4,12 +4,13 @@ const fs = require('fs');
 const path = require('path');
 const { binPath, currentDir } = require('../constants');
 
-const configTemplatePath = path.resolve(binPath, '../templates/.flowconfig');
-const flowTypedPath = path.resolve(binPath, '../templates/flow-typed');
+const configTemplatePath = path.resolve(binPath, './templates/.flowconfig');
+const flowTypedPath = path.resolve(binPath, './templates/flow-typed');
 const flowTypedDistPath = path.resolve(currentDir, 'flow-typed');
 const defaultDistPath = path.resolve(currentDir, '.flowconfig');
-const customDistPath = process.argv[2];
-const distPath = customDistPath || defaultDistPath;
+const argv = require('minimist')(process.argv.slice(2));
+
+const distPath = argv.p || argv.path || defaultDistPath;
 
 fs.symlink(configTemplatePath, distPath, () => {});
 fs.symlink(flowTypedPath, flowTypedDistPath, () => {});
